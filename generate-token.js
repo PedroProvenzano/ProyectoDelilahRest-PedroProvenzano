4597ac5816a260b8d5ae9d07c49542008dd79c25
Pedro Provenzano <pedroprov0@gmail.com>
2021-02-13 19:50:55 -0300

    Quick Fix

---
+++ generate-token.js
@@ -0,0 +1,6 @@
+console.log(
+  "ACCESS_TOKEN=" + require("crypto").randomBytes(64).toString("hex")
+);
+console.log(
+  "ACCESS_TOKEN_REFRESH=" + require("crypto").randomBytes(64).toString("hex")
+);
